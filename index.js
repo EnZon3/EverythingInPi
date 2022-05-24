5c9e0ce5213fe60e766c859173d394f7cf3fbc68
EnZon3 <37979038+EnZon3@users.noreply.github.com>
2022-05-23 22:19:38 -0400

    added some middleware

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // middleware
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
