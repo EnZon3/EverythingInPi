03877fe2ba0f0d53198078a5820788a845b9642a
EnZon3 <37979038+EnZon3@users.noreply.github.com>
2022-05-23 21:06:23 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const path = require('path');
+
+// middleware
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/html/index.html'));
+});
