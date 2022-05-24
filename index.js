9804c915099a583579d247a906cdd002c054cf7b
EnZon3 <37979038+EnZon3@users.noreply.github.com>
2022-05-24 19:56:07 -0400

    removed redundant bp lib bc bp is built into express

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const app = express();
 const port = process.env.PORT || 8080;
 const path = require('path');
 const fs = require('fs');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 // middleware
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 
